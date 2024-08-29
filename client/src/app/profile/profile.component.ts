5cc2e49c603cffe48fb093c9d8b7616136ba300d
Matej <matejspajic16@gmail.com>
2024-08-29 04:14:34 +0200

    start of profile component

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface DecodedToken {
+  username: string;
+  profile: string;
+  role: string;
+  email: string;
+}
 
 @Component({
-  selector: 'app-profile',
   standalone: true,
-  imports: [],
+  selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css'],
+  imports: [CommonModule]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: DecodedToken = { username: '', profile: '', role: '', email: '' };
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<{ token: string }>('/api/login').subscribe({
+      next: data => {
+        const decodedToken = this.decodeJWT(data.token) as DecodedToken;
+        this.user = decodedToken;
+        console.log(this.user);
+      },
+      error: error => {
+        console.error('Error fetching token:', error);
+      }
+    }
+    );
+  }
+
+  private decodeJWT(token: string): any {
+    const payload = token.split('.')[1];
+    const decodedPayload = atob(payload);
+    return JSON.parse(decodedPayload);
+  }
 }
